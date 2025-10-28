eebf4e8fd4493179d737a456f6e3f0f168d756c5
farhanintesar <farhanintesar0099@gmail.com>
2025-10-28 17:07:29 +0600

    25-61929-2

---
+++ Task2.java
@@ -0,0 +1,11 @@
+public class Task2 {
+    public static void main(String[] args) {
+        
+        int costs = 250 ;
+        System.out.println("Initial price: " + costs);
+        float discount = (costs* 0.15f);
+        System.out.print("After 15% discount: " + discount);
+
+    }
+    
+}
---
+++ Tsk1.java
@@ -0,0 +1,11 @@
+public class Tsk1 {
+     public static void main (String[] args){
+       int x = 80;
+       int y = 82;
+       int z = 84;
+       int sum = x + y + z;
+       System.out.println("Sum is : " + sum + " Average is : " + (sum/3));
+
+    }
+    
+}
