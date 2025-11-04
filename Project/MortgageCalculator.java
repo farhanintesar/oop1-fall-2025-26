cd6b2f590aa2c121f3c97e9c58ba6d231d3f55d8
farhanintesar <farhanintesar0099@gmail.com>
2025-11-04 19:29:01 +0600

    25-61929-2

--- Project/MortgageCalculator.java
+++ Project/MortgageCalculator.java
@@ -10,55 +10,37 @@ public class MortgageCalculator{
         System.out.print("Enter the load amount: ");
         int P = input.nextInt();
         System.out.print("Enter annual interest rate : ");
-        float r = input.nextFloat();
-        System.out.print("Enter Loan Period : ");
-        float n = input.nextFloat();
+        float R = input.nextFloat();
+        System.out.print("Enter Loan Period in Year : ");
+        int N = input.nextInt();
 
+        double r = (R / 100) / 12 ;
+        int n = N * 12 ;
 
-
-     
-        double M = P * ((r* Math.pow(1+r , n))/((Math.pow(1+r,n)) -1)); //total monthly mortgage amount
-        double totally = M * (n*12) ;                                 // monthly payment
-        double Interest = totally - M ;                                                         //total interest
-
-
-
+        double M = P * (r* Math.pow(1+r , (n)))/((Math.pow(1+r,(n)) -1)); //total monthly mortgage amount
+        double totalPayment = M * n ; 
+        double Interest = totalPayment - P ;                   
 
         System.out.println();
         System.out.println();
-
         System.out.println("===Mortgage Summery===");
 
 
-
         NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
-        String formattedValue = currencyFormatter.format(P);
+        
+        String formattedValue = currencyFormatter.format(P);              //done
         System.out.println("Loan Ammout : " + formattedValue);
 
-        String formattedMonthly = currencyFormatter.format(M);  //done
+        String formattedMonthly = currencyFormatter.format(M);            //done
         System.out.println("Monthly Payment : " + formattedMonthly);
 
-        String formattedYearly = currencyFormatter.format(totally);  //done
+        String formattedYearly = currencyFormatter.format(totalPayment);  //done
         System.out.println("Total Payment : " + formattedYearly);
 
-        String formattedInterest = currencyFormatter.format(Interest);
+        String formattedInterest = currencyFormatter.format(Interest);    //done
         System.out.println("Total Interest : " + formattedInterest);
         
 
-        
-        
-
+        input.close();
     }
-
-
 }
-
-
-
-
-
-
-
-
-    
-
